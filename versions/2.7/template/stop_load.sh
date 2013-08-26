82a0d671d8bca9e117528eac704e4258be4224e8
MC Brown <mc.brown@continuent.com>
2013-08-26 16:42:49 +0100

    Updating stop load

--- versions/2.7/template/stop_load.sh
+++ versions/2.7/template/stop_load.sh
@@ -2,6 +2,6 @@
 
 echo Killing any existing bristlecone processes
 
-ps -ef|grep bristlecone.evaluator|grep -v grep|awk '{ print $2; }'| xargs kill -n 9
-ps -ef|grep start_load.sh|grep -v grep|awk '{ print $2; }'| xargs kill -n 9
+ps -ef|grep bristlecone.evaluator|grep -v grep|awk '{ print $2; }'| xargs kill -s 9
+ps -ef|grep start_load.sh|grep -v grep|awk '{ print $2; }'| xargs kill -s 9
 
