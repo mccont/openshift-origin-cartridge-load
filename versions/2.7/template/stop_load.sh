d77671d22300bbf6e0aec40ff922732a273e9aac
MC Brown <mc.brown@continuent.com>
2013-12-09 16:52:20 +0000

    Fixing the killing the load

--- versions/2.7/template/stop_load.sh
+++ versions/2.7/template/stop_load.sh
@@ -9,6 +9,6 @@ then
     KILLCMD='kill -s kill'
 fi
 
-ps -ef|grep bristlecone.evaluator|grep -v grep|awk '{ print $2; }'| xargs $KILLCMD
 ps -ef|grep start_load.sh|grep -v grep|awk '{ print $2; }'| xargs $KILLCMD
+ps -ef|grep bristlecone.evaluator|grep -v grep|awk '{ print $2; }'| xargs $KILLCMD
 
